160cd3436c9403d33f0f3826fb4ed46134de0219
gleb lutsko <gleblutsko@gmail.com>
2023-08-10 15:32:22 +0300

    .

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,4 +4,3 @@ export function fetchBreeds() {
 export function fetchCatByBreed(breedId) {
     return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`).then(responce => responce.json())
 }
-
