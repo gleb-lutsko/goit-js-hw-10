79cc33bc5424722163befe7abe84f56fb985927a
gleb lutsko <gleblutsko@gmail.com>
2023-08-10 16:11:14 +0300

    доопрацювання

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
-export function fetchBreeds() {
-    return fetch('https://api.thecatapi.com/v1/breeds').then(responce => responce.json());
+export  function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_jPHvQ1Ha9Dr4V1GJoC2FPgdJjATKRRP9QbBENWCrnebKxRLr37qeEd9IDEzZECiD').then(responce => responce.json());
 }
-export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`).then(responce => responce.json())
+export  function fetchCatByBreed(breedId) {
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_jPHvQ1Ha9Dr4V1GJoC2FPgdJjATKRRP9QbBENWCrnebKxRLr37qeEd9IDEzZECiD`).then(responce => responce.json())
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import axios from "axios";
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
-axios.defaults.headers.common["x-api-key"] = "live_jPHvQ1Ha9Dr4V1GJoC2FPgdJjATKRRP9QbBENWCrnebKxRLr37qeEd9IDEzZECiD";
 const select = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const textLoader = document.querySelector('.loader');
@@ -8,6 +6,8 @@ const textError = document.querySelector('.error');
 
 hideElement(select);
 hideElement(textError);
+createOption();
+
 function createOption() {
     fetchBreeds()
         .then(data => {
@@ -17,28 +17,25 @@ function createOption() {
         .catch(() => showElement(textError))
         .finally(() => hideElement(textLoader));
 };
-createOption();
+
 select.addEventListener('change', onSelectCat);
+function onSelectCat(evt) {
+    fetchCatByBreed(evt.target.value)
+        .then(data => catInfo.innerHTML = createCatMarkup(data))
+        .catch(() => showElement(textError));
+}
+
+function createCatMarkup(arr) {
+    return arr.map(item => `<img src="${item.url}" width="300" alt="cat">
+        <h1>${item.breeds[0].name}</h1>
+        <p>${item.breeds[0].description}</p>
+        <p>Temperament: ${item.breeds[0].temperament}</p>
+    `).join('')
+}
+
 function showElement(element) {
     element.hidden = false;
 }
 function hideElement(element) {
     element.hidden = true;
-}
-function onSelectCat(evt) {
-    fetchBreeds().then(dataBreeds => {
-        fetchCatByBreed(evt.target.value)
-            .then(data => {
-            createCatMarkup(data[0].url, dataBreeds.find(cat => cat.id === evt.target.value))})
-            .catch(() => showElement(textError));
-    })
-}
-
-function createCatMarkup(url , { name, description,temperament}) {
-    catInfo.innerHTML = `
-    <img src="${url}" width="300" alt="cat">
-    <h1>${name}</h1>
-    <p>${description}</p>
-    <p>Temperament: ${temperament}</p>
-    `;
 }
